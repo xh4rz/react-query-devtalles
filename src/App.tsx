aa7d62f3da96aab02a1c2179b3d95a98bb8ca4b9
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2025-04-06 04:13:18 -0500

    02-Número aleatorio criptográfico atmosférico

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
+// https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new
+
 function App() {
-	console.log('Hola mundo');
+	const [number, setNumber] = useState(0);
+	const [isloading, setIsloading] = useState(true);
+	const [error, setError] = useState();
+
+	const [refreshtoken, setRefreshtoken] = useState(0);
+
+	useEffect(() => {
+		setIsloading(true);
+
+		fetch(
+			'https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new'
+		)
+			.then((resp) => resp.json())
+			.then((data) => setNumber(data))
+			.catch((error) => setError(error))
+			.finally(() => setIsloading(false));
+	}, [refreshtoken]);
 
 	return (
 		<>
-			<h1>Hola mundo</h1>
+			{isloading ? <h1>Cargando...</h1> : <h1>Numero: {number}</h1>}
+
+			<div>{error}</div>
+
+			<button
+				disabled={isloading}
+				onClick={() => setRefreshtoken(refreshtoken + 1)}
+			>
+				Nuevo número
+			</button>
 		</>
 	);
 }
